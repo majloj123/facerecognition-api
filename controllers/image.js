fc172d1ce2eb401ee1929c75e62a5dd79ac0d95c
Marian <marian.jandacka123@gmail.com>
2021-09-21 21:13:12 +0200

    My first commit

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+    apiKey: 'c0eefec476474bd0b42f9b66f7ca34d0'
+   });
+
+const handleApiCall = (req,res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json("unable to work with API"))
+}
+   
+const handleImage = (req,res,db) => {
+    const { id } = req.body;
+    db("users").where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("unable to count"));
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt-nodejs");
+const knex = require('knex');
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : '2682',
+      database : 'facerecognition'
+    }
+  });
+
+db.select("*").from("users").then(data => {
+    console.log(data);
+}); 
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const database = {
+    users: [
+        {
+            id: "123",
+            name: "John",
+            email: "johndoe@gmail.com",
+            password: "123456",
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: "124",
+            name: "Kathy",
+            email: "kathyrc@gmail.com",
+            password: "654321",
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+
+app.get("/", (req,res) => {
+    res.send(database.users)
+})
+
+app.post("/signin", (req,res) => { signin.handleSignIn(req,res,db,bcrypt)});
+
+app.post("/register", (req,res) => { register.handleRegister(req,res,db,bcrypt)});
+
+app.get("/profile/:id", (req, res) => { profile.handleProfile(req,res,db)})
+
+app.put("/image", (req,res) => { image.handleImage(req,res,db)})
+
+app.post("/imageurl", (req,res) => { image.handleApiCall(req,res)})
+
+app.listen(3000, () => {
+    console.log("app is running on port 3000")
+});
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user 
+*/
